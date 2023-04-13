3db1cc7c9c178b475919238fb44343efe87ea375
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2023-04-13 22:20:41 +1000

    created login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css';
+import { useStateContext } from './contexts/ContextProvider';
+import Auth from './pages/Auth'
 
 const App = () => {
   return (
-    <h1 className="underline text-3xl">App</h1>
+    <BrowserRouter>
+      <Auth/>
+    </BrowserRouter>
   )
 }
 
