1efe504728c7b2876f9104d34eefcca78df3485f
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2023-04-15 07:55:04 +1000

    Added page navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import PostJob from './pages/PostJob';
 import { Maker } from './pages/Maker';
+import { Sidebar } from './components/Sidebar';
 
 const theme = {
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
@@ -12,10 +13,12 @@ const theme = {
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' Component={PostJob}/>
-        <Route path='/maker' Component={Maker}/>
-      </Routes>
+      <Sidebar>
+        <Routes>
+          <Route path='/' Component={PostJob}/>
+          <Route path='/maker' Component={Maker}/>
+        </Routes>
+      </Sidebar>
     </BrowserRouter>
   )
 }
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { Drawer, Typography, List, ListItem, ListItemText, ListItemButton, } from '@mui/material'
+import { useTheme } from '@mui/material/styles';
+
+const drawerWidth = 240
+
+export const Sidebar = (props) => {
+
+    const theme = useTheme()
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const menuItems = [
+        {
+            text: 'Post a Job',
+            path: '/'
+        },
+        {
+            text: 'All Jobs',
+            path: '/maker'
+        },
+    ]
+
+    return (
+        <div className='flex'>
+            <Drawer
+                variant='permanent'
+                anchor='left'
+                PaperProps={{
+                    sx: {
+                        width: {
+                            md: drawerWidth,
+                            xs: drawerWidth/2
+                        }
+                    }
+                }}
+                sx={{
+                    width: {
+                        md: drawerWidth,
+                        xs: drawerWidth/2
+                    }
+                }}
+            >
+                <div>
+                    <Typography
+                        variant='h5'
+                        sx={{
+                            padding: theme.spacing(2)
+                        }}
+                    >
+                        Meyd.it
+                    </Typography>
+                </div>
+
+                {/* Lists / links */}
+                <List>
+                    {menuItems.map(item => {
+                        return <ListItemButton
+                            key={item.text}
+                            selected={location.pathname == item.path}
+                            onClick={(e) => {
+                                navigate(item.path)
+                            }}
+                        >
+                            <ListItemText 
+                                primary={item.text}
+                            />
+                        </ListItemButton>
+                    })}
+                </List>
+            </Drawer>
+            <div className='w-full p-8'>
+                {props.children}
+            </div>
+        </div>
+    )
+}
